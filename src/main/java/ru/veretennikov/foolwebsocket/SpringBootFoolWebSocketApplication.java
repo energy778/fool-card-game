ed7915a8c0335221a16fbe4f821d0d77fa72c2c4
energy 778 <one.million.dollars@mail.ru>
2020-05-03 00:12:16 +0300

    init commit

---
+++ src/main/java/ru/veretennikov/foolwebsocket/SpringBootFoolWebSocketApplication.java
@@ -0,0 +1,13 @@
+package ru.veretennikov.foolwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFoolWebSocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootFoolWebSocketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/veretennikov/foolwebsocket/SpringBootFoolWebSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.veretennikov.foolwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFoolWebSocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
