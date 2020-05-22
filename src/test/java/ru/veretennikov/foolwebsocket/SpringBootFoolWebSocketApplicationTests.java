873d02478cb2df400b744ccc96ba143777beefd2
energy 778 <one.million.dollars@mail.ru>
2020-05-22 08:14:46 +0300

    test fix bug, remove tests reports

--- src/test/java/ru/veretennikov/foolwebsocket/SpringBootFoolWebSocketApplicationTests.java
+++ src/test/java/ru/veretennikov/foolwebsocket/SpringBootFoolWebSocketApplicationTests.java
@@ -1,9 +1,7 @@
 package ru.veretennikov.foolwebsocket;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class SpringBootFoolWebSocketApplicationTests {
 
     @Test
